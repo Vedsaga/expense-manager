000437db8aba06240eecf293382d830053b71eea
harshit kumar <harshit.kumar@esamudaay.com>
2023-03-06 18:47:21 +0530

    updated the workflow based on suggestion by chatGPT

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -2,7 +2,7 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="kumzq27kR3tdymDQ3JbjlSngLaI7oR9V0OkOr5wyZv9dTdsQqR" \
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
 --output android/android_keys.zip android/android_keys.zip.gpg
 
 cd android
@@ -11,15 +11,3 @@ cd -
 
 # Copy release-keystore.jks and key.properties to the root directory of android
 cp android/keys/release-keystore.jks android/keys/key.properties android
-
-# Check if the files are copied successfully
-if [ -f android/release-keystore.jks ] && [ -f android/key.properties ]; then
-  echo "Files copied successfully."
-else
-  echo "Failed to copy files."
-fi
-
-FILE=./android/release-keystore.jks
-if test -f "$FILE"; then
-    echo "$FILE exists."
-fi
