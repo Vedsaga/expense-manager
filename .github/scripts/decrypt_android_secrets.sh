e9c0cb6a24f1fbf8de537c7972446d374c6f063c
harshit kumar <harshit.kumar@esamudaay.com>
2023-03-06 17:52:08 +0530

    test workflow

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -2,17 +2,16 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="kumzq27kR3tdymDQ3JbjlSngLaI7oR9V0OkOr5wyZv9dTdsQqR" \
 --output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && cd -
 echo $PWD
 
 echo "********1"
 ls -d $PWD/android/*
-mv ./android/expensemanager.jks ./android/app
-mv ./android/google-services.json ./android/app
+mv ./android/release-keystore.jks ./android/app
 echo ./
 echo "********2"
-FILE=./android/app/expensemanager.jks
+FILE=./android/app/release-keystore.jks
 if test -f "$FILE"; then
     echo "$FILE exists."
 fi
